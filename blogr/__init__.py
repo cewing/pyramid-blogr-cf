73f3c074039ad9a62f320dfc4c079c19cb216eba
cewing <cris@crisewing.com>
2014-07-30 17:20:24 -0700

    correct a typo in the name of our templating engine

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -13,7 +13,7 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja')
+    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('blog', '/blog/{id:\d+}/{slug}')
