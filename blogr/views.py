2905b7fbc5ff5c723328b59113d7960e90036e19
cewing <cris@crisewing.com>
2014-07-30 17:20:47 -0700

    First, we prepare stub view methods for the endpoints we'll need to serve.
    At the same time, we create stub templates to stand in for the templates we'll eventually need to flesh out.  This allows us to verify that our routes are properly working before we attempt to go any further
    
    We should also remove the unused template created by our `sqlalchemy` scaffold.  This keeps our project directory clean of unused code.

--- blogr/views.py
+++ blogr/views.py
@@ -6,3 +6,32 @@ from sqlalchemy.exc import DBAPIError
 from .models import (
     DBSession,
     )
+
+
+@view_config(route_name='home', renderer='blogr:templates/index.jinja2')
+def index_page(request):
+    return {}
+
+
+@view_config(route_name='blog', renderer='blogr:templates/view_blog.jinja2')
+def blog_view(request):
+    return {}
+
+
+@view_config(route_name='blog_action', match_param='action=create',
+             renderer='blogr:templates/edit_blog.jinja2')
+def blog_create(request):
+    return {}
+
+
+@view_config(route_name='blog_action', match_param='action=edit',
+             renderer='blogr:templates/edit_blog.jinja2')
+def blog_update(request):
+    return {}
+
+
+@view_config(route_name='auth', match_param='action=in', renderer='string',
+             request_method='POST')
+@view_config(route_name='auth', match_param='action=out', renderer='string')
+def sign_in_out(request):
+    return {}
