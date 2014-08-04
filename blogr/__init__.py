0daa4e793025f5477185ec28f88b4af0581746b0
cewing <cris@crisewing.com>
2014-08-04 16:50:34 -0700

    Add routes for our application that will allow for:
    * Viewing an individual blog post
    * Adding a new post or editing one that already exists
    * Logging in and out of the application
    
    These URL *endpoints* will define the functionality available in our application.

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -17,5 +17,8 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('auth', '/sign/{action}')
     config.scan()
     return config.make_wsgi_app()
