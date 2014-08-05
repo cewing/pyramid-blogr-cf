2f64b75e3c2bbb091ad5abf5c8f176fcb1069165
cewing <cris@crisewing.com>
2014-08-04 17:37:22 -0700

    We start our view work by creating the code to view a paginated list of blog entries and the code for viewing an individual blog entry.
    We also need to add a template that will provide the entire site-wide layout.  Our other templates will extend this one.
    
    Next, We create the template that will show us a single blog entry.
    
    And finally, We create the template that will show us a paginated list of blog entries.

--- blogr/views.py
+++ blogr/views.py
@@ -1,21 +1,26 @@
-from pyramid.response import Response
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.view import view_config
-
-from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
+    Entry,
     )
 
 
 @view_config(route_name='home', renderer='blogr:templates/index.jinja2')
 def index_page(request):
-    return {}
+    page = int(request.params.get('page', 1))
+    paginator = Entry.paginator(request, page)
+    return {'paginator': paginator}
 
 
 @view_config(route_name='blog', renderer='blogr:templates/view_blog.jinja2')
 def blog_view(request):
-    return {}
+    id = int(request.matchdict.get('id', -1))
+    entry = Entry.by_id(id)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
 
 
 @view_config(route_name='blog_action', match_param='action=create',
