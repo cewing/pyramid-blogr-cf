41826a9ad416c7df2a75ea9283059c94a9c7fda4
cewing <cris@crisewing.com>
2015-01-02 22:07:07 -0800

    update initialize_db script for heroku compatibility

--- blogr/scripts/initializedb.py
+++ blogr/scripts/initializedb.py
@@ -34,6 +34,8 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
+    if 'DATABASE_URL' in os.environ:
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
