dec7c53357b24b2f340c830336ad5f9d0b06af15
cewing <cris@crisewing.com>
2016-10-04 18:16:02 -0700

    define the User and BlogRecord models
    These models will represent the entities in our system we persist.

--- blogr/models/blog_record.py
+++ blogr/models/blog_record.py
@@ -9,3 +9,12 @@ from sqlalchemy import (
     UnicodeText,
     DateTime,
 )
+
+
+class BlogRecord(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
--- blogr/models/user.py
+++ blogr/models/user.py
@@ -6,6 +6,13 @@ from sqlalchemy import (
     Column,
     Integer,
     Unicode,
-    UnicodeText,
     DateTime,
 )
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    password = Column(Unicode(255), nullable=False)
+    last_logged = Column(DateTime, default=datetime.datetime.utcnow)
