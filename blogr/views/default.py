8128544a3fc8fc5f0acb28e6c21a4ed6f0507c1d
cewing <cris@crisewing.com>
2016-10-04 18:32:17 -0700

    Update default view to show default user
    Since MyModel no longer exists, we have to replace it with the User model.  We'll grab the first one from our database and show it instead.

--- blogr/views/default.py
+++ blogr/views/default.py
@@ -3,14 +3,14 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import MyModel
+from ..models import User
 
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
     try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
+        query = request.dbsession.query(User)
+        one = query.filter(User.name == 'admin').first()
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
     return {'one': one, 'project': 'blogr'}
