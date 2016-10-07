6297ff86163aa05932a7ce465dd282210d440b2b
cewing <cris@crisewing.com>
2016-10-06 19:05:31 -0700

    add an index view
    This view will display a list of blog records.
    
    The list will be paginated.
    
    if there is no list, we'll see a statement to that effect.
    
    There will also be a link to the page for creating a new entry
    
    At this point, we should be able to start our application and see the home page. Try loading http://localhost:6543

--- blogr/views/default.py
+++ blogr/views/default.py
@@ -1,9 +1,14 @@
 from pyramid.view import view_config
 
+from ..services.blog_record import BlogRecordService
 
-@view_config(route_name='home', renderer='string')
+
+@view_config(route_name='home',
+             renderer='blogr:templates/index.jinja2')
 def index_page(request):
-    return 'Index Page'
+    page = int(request.params.get('page', 1))
+    paginator = BlogRecordService.get_paginator(request, page)
+    return {'paginator': paginator}
 
 
 @view_config(route_name='auth', match_param='action=in', renderer='string',
