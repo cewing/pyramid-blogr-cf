fcb1a6e2caf179dcd91c306a6f874d7afce3e84e
cewing <cris@crisewing.com>
2016-10-06 21:20:12 -0700

    Update the edit and create views for blog records
    The create view uses our BlogCreateForm to validate data from an incoming request and transfer it to a new BlogRecord object.
    
    The edit view uses the blog service to retrieve an existing entry by ID, and then updates that with data from a request validated by the BlogUpdateForm
    
    Both form views only process data that has been passed in a POST request.

--- blogr/views/blog.py
+++ blogr/views/blog.py
@@ -3,6 +3,7 @@ from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 
 from ..models.blog_record import BlogRecord
 from ..services.blog_record import BlogRecordService
+from ..forms import BlogCreateForm, BlogUpdateForm
 
 
 @view_config(route_name='blog',
@@ -16,12 +17,27 @@ def blog_view(request):
 
 
 @view_config(route_name='blog_action', match_param='action=create',
-             renderer='string')
+             renderer='blogr:templates/edit_blog.jinja2')
 def blog_create(request):
-    return 'Create a New Blog Record'
+    entry = BlogRecord()
+    form = BlogCreateForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        request.dbsession.add(entry)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form, 'action': request.matchdict.get('action')}
 
 
 @view_config(route_name='blog_action', match_param='action=edit',
-             renderer='string')
+             renderer='blogr:templates/edit_blog.jinja2')
 def blog_update(request):
-    return 'Edit a Blog Record'
+    blog_id = int(request.params.get('id', -1))
+    entry = BlogRecordService.by_id(blog_id, request)
+    if not entry:
+        return HTTPNotFound()
+    form = BlogUpdateForm(request.POST, entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        return HTTPFound(
+            location=request.route_url('blog', id=entry.id,slug=entry.slug))
+    return {'form': form, 'action': request.matchdict.get('action')}
