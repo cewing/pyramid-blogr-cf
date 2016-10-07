f4dadf99f9bfde934b39126e6e9c5ca3ae31cf8f
cewing <cris@crisewing.com>
2016-10-06 19:11:28 -0700

    5 -- Add view of a single blog entry
    This new view will retrieve an entry based on the ID that is matched in the route.
    
    If no entry is found with that ID, then we will return a 404 Not Found response

--- blogr/views/blog.py
+++ blogr/views/blog.py
@@ -1,9 +1,18 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
+
+from ..models.blog_record import BlogRecord
+from ..services.blog_record import BlogRecordService
 
 
-@view_config(route_name='blog', renderer='string')
+@view_config(route_name='blog',
+             renderer='blogr:templates/view_blog.jinja2')
 def blog_view(request):
-    return 'View a Blog Record'
+    blog_id = int(request.matchdict.get('id', -1))
+    entry = BlogRecordService.by_id(blog_id, request)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
 
 
 @view_config(route_name='blog_action', match_param='action=create',
