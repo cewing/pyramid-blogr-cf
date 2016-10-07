7e0f77c763fc3928247333d60229446484e3b23a
cewing <cris@crisewing.com>
2016-10-06 18:59:01 -0700

    Add utility properties to our blog model
    The  property serves to allow us to place the title of a blog record in our url in a normalized way.
    
    The  property provides the date and time when a blog record was created in words, like 'yesterday' or 'three weeks ago'

--- blogr/models/blog_record.py
+++ blogr/models/blog_record.py
@@ -1,4 +1,6 @@
 import datetime
+from webhelpers2.text import urlify #<- will generate slugs
+from webhelpers2.date import distance_of_time_in_words #<- human friendly dates
 
 from blogr.models.meta import Base
 
@@ -18,3 +20,12 @@ class BlogRecord(Base):
     body = Column(UnicodeText, default=u'')
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @property
+    def slug(self):
+        return urlify(self.title)
+
+    @property
+    def created_in_words(self):
+        return distance_of_time_in_words(self.created,
+                                         datetime.datetime.utcnow())
