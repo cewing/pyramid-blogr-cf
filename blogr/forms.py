9cc6908ed0b812b1743d2b796c7a1620ab9eb690
cewing <cris@crisewing.com>
2016-10-08 15:39:15 -0700

    Add a registration form

--- blogr/forms.py
+++ blogr/forms.py
@@ -1,5 +1,5 @@
 from wtforms import Form, StringField, TextAreaField, validators
-from wtforms import HiddenField
+from wtforms import HiddenField, PasswordField
 
 strip_filter = lambda x: x.strip() if x else None
 
@@ -13,3 +13,9 @@ class BlogCreateForm(Form):
 
 class BlogUpdateForm(BlogCreateForm):
     id = HiddenField()
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=1, max=255)],
+                           filters=[strip_filter])
+    password = PasswordField('Password', [validators.Length(min=3)])
