df03e277fcf990e09ca885fc675084d34ad9662e
cewing <cris@crisewing.com>
2016-10-08 18:14:59 -0700

    10 -- Hide edit link from others
    By adding the BlogRecordFactory as the factory for the blog view as well, we can then check for the edit permission in the context of a template.
    If the current user is the author of the post we are viewing, then they will be able to see the 'edit' button.
    Otherwise, it will be hidden from them

--- blogr/routes.py
+++ blogr/routes.py
@@ -1,7 +1,8 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}',
+                     factory='blogr.security.BlogRecordFactory')
     config.add_route('blog_action', '/blog/{action}',
                      factory='blogr.security.BlogRecordFactory')
     config.add_route('auth', '/sign/{action}')
